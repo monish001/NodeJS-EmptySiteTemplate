e1e9f4d88d8dc44cc2f5997168f026d148c1f703
Monish Gupta <monish.gupta1@gmail.com>
2018-11-14 18:55:28 +0530

    logs test

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
-    
+    console.log('logs go here!!');
+    console.error('errors go here!!');
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end('Hello, world!!!');
+
     
 }).listen(process.env.PORT || 8080);

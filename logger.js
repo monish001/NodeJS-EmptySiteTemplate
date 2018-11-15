3259ebb5e25c24ae4765c7b24d472cfa3fd8ea36
Monish Gupta <monish.gupta1@gmail.com>
2018-11-15 10:18:41 +0530

    logger test

---
+++ logger.js
@@ -0,0 +1,20 @@
+var winston = require("winston");
+  require("winston-azure-blob-transport");
+ 
+  var logger = new (winston.Logger)({
+    transports: [
+      new (winston.transports.AzureBlob)({
+        account: {
+          name: "superbot002",
+          key: "u2WnCmjoo5VyWt5MsgA48u5cNnX853qJjLP8C9i3WkNV917QBWNxmEMoZKF1LjzftDTlaV7wivsL+Z86U0TGCA=="
+        },
+        containerName: "app-logs-rw",
+        blobName: "superbot002",
+        level: "info"
+      })
+    ]
+  });
+  
+  logger.warn("Test from logger.js!");
+
+  module.exports = logger;
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var http = require('http');
-
+var logger = require('./logger');
 http.createServer(function (req, res) {
     console.log('logs go here!!');
     console.error('errors go here!!');
+    logger.warn('Test winston logger!!');
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end('Hello, world!!!');
 
